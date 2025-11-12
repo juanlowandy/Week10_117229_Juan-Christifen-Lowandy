8f508d6146ac66cf75b0b55fb9322f7856a3f452
Juan <juanlowandy@gmail.com>
2025-11-12 11:25:26 +0700

    Final Keyword

---
+++ src/week10_juan_117229/a.java
@@ -0,0 +1,9 @@
+package week10_juan_117229;
+
+final class a {
+
+	final void method1()
+	{
+		System.out.println("This is a final method");
+	}
+}
---
+++ src/week10_juan_117229/b.java
@@ -0,0 +1,10 @@
+package week10_juan_117229;
+
+public class b extends a {
+     
+	 void method1()
+	 {
+		 // Compile-error! We can not override
+		 System.out.println("Illegal!");
+	 }
+}
